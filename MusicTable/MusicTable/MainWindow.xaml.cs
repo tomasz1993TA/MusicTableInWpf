0f4ffef7d105c335bc2615579677f211238a8a7d
Tomasz Anuszczyk <tomaszanuszczyk@gmail.com>
2018-12-19 16:19:06 +0100

    Supplemented events

--- MusicTable/MusicTable/MainWindow.xaml.cs
+++ MusicTable/MusicTable/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using MusicTable.Models;
 
 namespace MusicTable
@@ -11,7 +12,8 @@ namespace MusicTable
     /// </summary>
     public partial class MainWindow : Window
     {
-        MusicDatabaseEntities db = new MusicDatabaseEntities();
+        MusicDatabaseEntities db = new MusicDatabaseEntities();        
+        
         public MainWindow()
         {
             InitializeComponent();                    
@@ -24,7 +26,20 @@ namespace MusicTable
 
         private void AddNewSongB_Click(object sender, RoutedEventArgs e)
         {
-            
+            Models.MusicTable mtdb = new Models.MusicTable()
+            {
+                IdNumber = IdNumberTB.Text,
+                SongName = SongNameTB.Text,
+                Album = AlbumTB.Text,
+                Band = BandTB.Text,
+                Author = AuthorTB.Text,
+                ReleaseDate = ReleaseDateTB.Text,
+                Length = LengthTB.Text
+            };
+
+            db.MusicTable.Add(mtdb);
+            db.SaveChanges();
+            DataGridXaml.ItemsSource = db.MusicTable.ToList();
         }             
         
         private void ClearTextBoxesB_Click(object sender, RoutedEventArgs e)
@@ -36,6 +51,46 @@ namespace MusicTable
             AuthorTB.Text = String.Empty;
             ReleaseDateTB.Text = String.Empty;
             LengthTB.Text = String.Empty;
-        }               
+        }
+
+        private void DeleteB_Click(object sender, RoutedEventArgs e)
+        {
+            Models.MusicTable rowSelected = (Models.MusicTable)DataGridXaml.SelectedItem;
+            
+            db.MusicTable.Remove(rowSelected);
+            db.SaveChanges();
+            DataGridXaml.ItemsSource = db.MusicTable.ToList();
+        }
+
+        private void DataGridXaml_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            Models.MusicTable rowSelected = (Models.MusicTable)DataGridXaml.SelectedItem;
+
+            if (rowSelected != null)
+            {
+                IdNumberTB.Text = rowSelected.IdNumber;
+                SongNameTB.Text = rowSelected.SongName;
+                AlbumTB.Text = rowSelected.Album;
+                BandTB.Text = rowSelected.Band;
+                AuthorTB.Text = rowSelected.Author;
+                ReleaseDateTB.Text = rowSelected.ReleaseDate;
+                LengthTB.Text = rowSelected.Length;
+            }            
+        }
+
+        private void EditB_Click(object sender, RoutedEventArgs e)
+        {
+            Models.MusicTable rowSelected = (Models.MusicTable)DataGridXaml.SelectedItem;
+
+            rowSelected.IdNumber = IdNumberTB.Text;
+            rowSelected.SongName = SongNameTB.Text;
+            rowSelected.Album = AlbumTB.Text;
+            rowSelected.Band = BandTB.Text;
+            rowSelected.Author = AuthorTB.Text;
+            rowSelected.ReleaseDate = ReleaseDateTB.Text;
+            rowSelected.Length = LengthTB.Text;
+
+            DataGridXaml.ItemsSource = db.MusicTable.ToList();
+        }
     }
 }
